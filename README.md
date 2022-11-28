015aa97b34aa03b71787078b7823ef8e02fb7fb2
zhang.xin <zhang.xin@hzleaper.com>
2022-11-28 17:15:55 +0800

    feat: add windwos canal server start

--- README.md
+++ README.md
@@ -4,20 +4,40 @@ canal 自动自动安装脚本 跨平台支持  win + linux
 
 ## 配置
 
+### 1, 目标机器有 node + java 的环境
+
+### 2, 监控的mysql 开启binlog
+
+### 3, 配置的模式是kafka,需要 安装kafka 和 zookeper
+
+
 ```json
 
 {
     "taget_path": "D:/hzleaper_auto_install/canal",
     "inspect_mysql_config": {
-        "host": "192.168.0.132",
+        "host": "127.0.0.1",
         "port": 3306,
         "user": "canal",
         "password": "canal"
     },
-    "kafka": "192.168.0.132:9092",
-    "mq_topic": "example",
-    "zookeeper": "192.168.0.132:2181"
+    "kafka": {
+        "ip": "101.43.105.27",
+        "port": 9092
+    },
+    "mq_topic": "pol_c",
+    "zookeeper": {
+        "ip": "101.43.105.27",
+        "port": 2181
+    },
+    "canal_config": {
+        "ip": "127.0.0.1",
+        "port": 11111,
+        "canal_metrics_pull_port": 11112,
+        "canal_admin_port": 11110
+    }
 }
+
 
 ```
 
@@ -27,5 +47,15 @@ canal 自动自动安装脚本 跨平台支持  win + linux
 
 ## 目录结构
 
-开发中....
+```text
+canal-install-package
+ ├─ package
+ │ ├─ canal // canal 源文件包
+ │ ├─ config_template // 配置模板
+ │ │ ├─ canal_example_instance_template.properties
+ │ │ └─ canal_template.properties
+ │ └─ index.js // 入口
+ ├─ config.json
+ └─ install.js
+```
 
--- package/index.js
+++ package/index.js
@@ -1,42 +1,142 @@
-const { zip } = require('compressing');
+const os = require('os');
 const fs = require('fs-extra');
-const { join } = require('path');
-const { exec } = require('shelljs');
 const readline = require('readline');
-const { taget_path } = require('../config.json');
+const { zip } = require('compressing');
+const { join } = require('path');
+const { exec, cd } = require('shelljs');
+const { taget_path, inspect_mysql_config, kafka, mq_topic, zookeeper, canal_config } = require('../config.json');
 
-const SOURCE_PATH = join(__dirname, './canal-package');
+const OS_TYPE = os.type() == 'Windows_NT' ? 'win' : 'linux';
 const TARGET_DIR = taget_path;
 const CANAL_TEMPLATE_PATH = join(__dirname, './config_template/canal_template.properties');
 const CANAL_EXAMPLE_INSTANCE_PATH = join(__dirname, './config_template/canal_example_instance_template.properties');
-const CANAL_TARGET_PATH = join(__dirname, './canal-package/conf/canal.properties');
-const CANAL_EXAMPLE_TARGET_PATH = join(__dirname, './canal-package/conf/example/instance.properties');
+const CANAL_TARGET_PATH = join(__dirname, './canal/conf/canal.properties');
+const CANAL_EXAMPLE_TARGET_PATH = join(__dirname, './canal/conf/example/instance.properties');
 
 const CONFIG_LIST = [
     {
+        desc: 'cannal instance config',
         tempPath: CANAL_EXAMPLE_INSTANCE_PATH,
         targetPath: CANAL_EXAMPLE_TARGET_PATH,
+        confCertification: {
+            master_address: (line) => {
+                if (line.startsWith('canal.instance.master.address')) {
+                    fs.appendFileSync(
+                        CANAL_EXAMPLE_TARGET_PATH,
+                        `canal.instance.master.address=${inspect_mysql_config.host}:${inspect_mysql_config.port} \n`,
+                    );
+                    return true;
+                }
+                return false;
+            },
+            canal_db_user: (line) => {
+                if (line.startsWith('canal.instance.dbUsername')) {
+                    fs.appendFileSync(
+                        CANAL_EXAMPLE_TARGET_PATH,
+                        `canal.instance.dbUsername=${inspect_mysql_config.user} \n`,
+                    );
+                    return true;
+                }
+                return false;
+            },
+            canal_db_password: (line) => {
+                if (line.startsWith('canal.instance.dbPassword')) {
+                    fs.appendFileSync(
+                        CANAL_EXAMPLE_TARGET_PATH,
+                        `canal.instance.dbPassword=${inspect_mysql_config.password} \n`,
+                    );
+                    return true;
+                }
+                return false;
+            },
+            canal_mq_topic: (line) => {
+                if (line.startsWith('canal.mq.topic')) {
+                    fs.appendFileSync(CANAL_EXAMPLE_TARGET_PATH, `canal.mq.topic=${mq_topic} \n`);
+                    return true;
+                }
+                return false;
+            },
+        },
     },
     {
+        desc: 'cannal config',
         tempPath: CANAL_TEMPLATE_PATH,
         targetPath: CANAL_TARGET_PATH,
+        confCertification: {
+            canal_ip: (line) => {
+                if (line.startsWith('canal.ip')) {
+                    fs.appendFileSync(CANAL_TARGET_PATH, `canal.ip=${canal_config.ip} \n`);
+                    return true;
+                }
+                return false;
+            },
+            canal_register_ip: (line) => {
+                if (line.startsWith('canal.register.ip')) {
+                    fs.appendFileSync(CANAL_TARGET_PATH, `canal.register.ip=${canal_config.ip} \n`);
+                    return true;
+                }
+                return false;
+            },
+            canal_zkServers: (line) => {
+                if (line.startsWith('canal.zkServers')) {
+                    fs.appendFileSync(CANAL_TARGET_PATH, `canal.zkServers=${zookeeper.ip}:${zookeeper.port} \n`);
+                    return true;
+                }
+                return false;
+            },
+            canal_destinations: (line) => {
+                if (line.startsWith('canal.destinations')) {
+                    fs.appendFileSync(CANAL_TARGET_PATH, `canal.destinations=${mq_topic} \n`);
+                    return true;
+                }
+                return false;
+            },
+            kafka_bootstrap_servers: (line) => {
+                if (line.startsWith('kafka.bootstrap.servers ')) {
+                    fs.appendFileSync(CANAL_TARGET_PATH, `kafka.bootstrap.servers=${kafka.ip}:${kafka.port} \n`);
+                    return true;
+                }
+                return false;
+            },
+        },
     },
 ];
 
 async function main() {
     try {
         fs.ensureDirSync(TARGET_DIR);
-
-        for (let i = 0; i < CONFIG_LIST.length; i++) {
-            const { tempPath, targetPath } = CONFIG_LIST[i];
-            await updateRedisConfig(tempPath, targetPath);
-        }
+        await handleConfig();
+        console.log(`start copy to ${TARGET_DIR} ...`);
+        await moveToTargetDir();
+        console.log(`start open service ...`);
+        startService();
+        console.log(`service open success!`);
     } catch (_) {
         console.log(_);
     }
 }
 
-async function updateRedisConfig(tempPath, targetPath) {
+async function handleConfig() {
+    for (let i = 0; i < CONFIG_LIST.length; i++) {
+        console.log(`update${CONFIG_LIST[i].desc}...`);
+        await updateRedisConfig(CONFIG_LIST[i]);
+    }
+}
+
+function startService() {
+    if (OS_TYPE == 'win') {
+        cd(`${TARGET_DIR}/canal/bin`);
+        exec(`CanalService.exe stop`);
+        exec(`CanalService.exe uninstall`);
+        exec(`CanalService.exe install`);
+        exec(`CanalService.exe start`);
+        return;
+    }
+    exec(`${TARGET_DIR}/canal/bin/stop.sh`);
+    exec(`${TARGET_DIR}/canal/bin/startup.sh`);
+}
+
+async function updateRedisConfig({ tempPath, targetPath, confCertification }) {
     return new Promise((resolve, reject) => {
         try {
             fs.writeFileSync(targetPath, '');
@@ -51,14 +151,25 @@ async function updateRedisConfig(tempPath, targetPath) {
         });
 
         rl.on('line', (line) => {
-            if (line.startsWith('logfile')) {
-                return fs.appendFileSync(targetPath, `logfile "${log_file}" \n`);
-            }
-            fs.appendFileSync(targetPath, `${line} \n`);
+            appConfiguration(line, confCertification, targetPath);
         });
 
         rl.on('close', resolve(true));
     });
 }
 
-module.exports = { main, updateRedisConfig };
+function appConfiguration(line, confCertification, targetPath) {
+    const confRulesList = Object.values(confCertification);
+    for (let i = 0; i < confRulesList.length; i++) {
+        if (confRulesList[i](line)) return;
+    }
+    return fs.appendFileSync(targetPath, `${line} \n`);
+}
+
+async function moveToTargetDir() {
+    const canelDir = join(__dirname, './canal');
+    await zip.compressDir(canelDir, `${TARGET_DIR}.zip`);
+    await zip.uncompress(`${TARGET_DIR}.zip`, `${TARGET_DIR}`);
+}
+
+module.exports = { main };
